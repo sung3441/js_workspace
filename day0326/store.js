48e551e597e8a52fdd69ff2657ee8b1fcf6646d2
sungil <whtjddlf56@naver.com>
2021-03-26 19:26:47 +0900

    day0326

---
+++ day0326/store.js
@@ -0,0 +1,54 @@
+/*
+역삼동 맛집 정보 json
+*/ 
+var store = {
+    "local" : "역삼동",
+    "theme" : "맛집",
+    "storeList" : [
+        {
+            "comment" : "떡볶이 존맛;;",
+            "name" : "밀빵떡볶이",
+            "latlng" : {
+                "lat" : 37.50091705137511, 
+                "lng" : 127.03310937358229
+            },
+            "img" :  "./image/ricecake.jpg"
+        },
+        {
+            "comment" : "후라이팬존맛;;",
+            "name" : "더후라이팬",
+            "latlng" : {
+                "lat":37.501294236579184, 
+                "lng":127.0330118102085,
+            },
+            "img" :  "./image/fripan.jpg"
+        },
+        {
+            "comment" : "삼곱존맛;;",
+            "name" : "삼곱식당",
+            "latlng" : {
+                "lat":37.502582,
+                "lng":127.037233,
+            },
+            "img" :  "./image/samgob.jpg"
+        },
+        {
+            "comment" : "부대지께존맛;;",
+            "name" : "이태리부대찌게",
+            "latlng" : {
+                "lat":37.502935868017275, 
+                "lng":127.03815809523849,
+            },
+            "img" :  "./image/italy.jpg"
+        },
+        {
+            "comment" : "우리집 존맛;;",
+            "name" : "우리집",
+            "latlng" : {
+                "lat":37.503329079071506, 
+                "lng":127.03884020356904,
+            },
+            "img" :  "./image/woori.jpg"
+        }             
+    ]
+}
